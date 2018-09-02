09a74cc65019e50abeb99446fc8af3c249af0ba5
Mitja Bezensek <bezensek.mitja@gmail.com>
2018-09-02 22:39:51 +0200

    Add sensitivity. Add Makefile.

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
 UPLOAD_PORT = /dev/tty.SLAB_USBtoUART
-
 include $(HOME)/Documents/Arduino/makeEspArduino/makeEspArduino.mk
--- power_reader.ino
+++ power_reader.ino
@@ -3,22 +3,43 @@
 #define HEATER_PIN 0
 #define HEAT_PUMP_PIN 2
 
+// will not send to emoncms
+#define NO_EMONCMS
+
+// how long to wait before a new signal is detected
+#define SIGNAL_SENSITIVITY_MS 200
+
 unsigned long lastUpdate;
 
 // send every minute
 #define SEND_MILLIS 60000
 
-#define WH(X) (X)*100
-
 volatile unsigned long countHeater = 0;
 volatile unsigned long countHeatPump = 0;
+unsigned long lastHeaterUpdate = 0;
+unsigned long lastHeatPumpUpdate = 0;
+
+bool shouldUpdate(unsigned long &lastUpdate) {
+    unsigned long now = millis();
+    if (now - lastUpdate < SIGNAL_SENSITIVITY_MS)
+        return false;
+
+    lastUpdate = now;
+    return true;
+}
 
 void doCountHeater() {
+    if (!shouldUpdate(lastHeaterUpdate))
+        return;
+
     countHeater++;
 	Serial.println("Heater 100 Wh");
 }
 
 void doCountHeatPump() {
+    if (!shouldUpdate(lastHeatPumpUpdate))
+        return;
+
     countHeatPump++;
 	Serial.println("Heat pump 100 Wh");
 }
@@ -48,7 +69,11 @@ void loop() {
 	unsigned long now = millis();
 	if (now - lastUpdate > SEND_MILLIS) {
 		lastUpdate = now;
+        #ifdef NO_EMONCMS
+        Serial.println("Would send to emoncms.");
+        #else
 		sendPowerUsage(countHeater, countHeatPump);
+        #endif
 	}
 
     delay(1000);
